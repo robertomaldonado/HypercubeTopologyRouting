0855fb39bf9c3efaa6ba446b79b012fcd6762bfe
Roberto <robertomaldonadog@hotmail.com>
2019-02-02 13:24:39 -0500

    Refactoring for the map creation was done

--- links.cpp
+++ links.cpp
@@ -13,6 +13,8 @@ using namespace std;
 
 int dim_order_routing(int src, int dst, int *path); /* return path length */
 int allpath_routing(int src, int dst, int allpath[MAX_PATH][MAXZ_PATH_LEN]); /* return number of paths */
+void print_binary_map( map<int, vector<int> > & nodes_map, int n_size);
+void create_map( map<int, vector<int> > & nodes_map , int n_size);
 
 int main(int argc, char * argv[]){
     //Check arguments for right input
@@ -23,10 +25,48 @@ int main(int argc, char * argv[]){
     if ( !isValidOption(routing_model) ) return 1;
 
     //Create a vector to hold a binary representation
+    map<int, vector<int> > nodes_map;
+    create_map( nodes_map , n_size);
+
+    print_binary_map(nodes_map, n_size);
+    // for(auto nodes_map_it = nodes_map.begin(); nodes_map_it != nodes_map.end(); ++nodes_map_it){
+    //     //Print the node id
+    //     bitConversion( log2(n_size), nodes_map_it->first);
+    //     cout << ": ";
+    //     //Print the neighbors
+    //     for(auto nei_it = nodes_map_it->second.begin(); nei_it != nodes_map_it->second.end(); ++nei_it){
+    //         bitConversion( log2(n_size), *nei_it);
+    //         if(nei_it+1 != nodes_map_it->second.end() )
+    //             cout<<" ";
+    //     }
+    //     cout << endl;
+    // }
+
+    return 0;
+}
+
+void print_map(map<int, vector<int> > &nodes_map , int n_size){
+
+    for(auto nodes_map_it = nodes_map.begin(); nodes_map_it != nodes_map.end(); ++nodes_map_it){
+        //Print the node id
+        bitConversion( log2(n_size), nodes_map_it->first);
+        cout << ": ";
+        //Print the neighbors
+        for(auto nei_it = nodes_map_it->second.begin(); nei_it != nodes_map_it->second.end(); ++nei_it){
+            bitConversion( log2(n_size), *nei_it);
+            if(nei_it+1 != nodes_map_it->second.end() )
+                cout<<" ";
+        }
+        cout << endl;
+    }
+}
+
+void create_map(  map<int, vector<int> > &nodes_map , int n_size){
+
     vector<int> vector_nodes;
     vector<int> x_ors;
     vector<int> tmp_neighbors;
-    map<int, vector<int> > grid;
+
     //Initialize the vector for nodes
     for(int i=0; i < n_size; i++)
         vector_nodes.push_back(i);
@@ -42,22 +82,9 @@ int main(int argc, char * argv[]){
         for(int i=0; i < x_ors.size(); i++)
             tmp_neighbors.push_back( vector_nodes[j]^x_ors[i] );
         //Add node and neigborg to map
-        grid[ vector_nodes[j] ] = tmp_neighbors;
+        nodes_map[ vector_nodes[j] ] = tmp_neighbors;
         tmp_neighbors.clear();
     }
     vector_nodes.clear();
-
-    for(auto grid_it = grid.begin(); grid_it != grid.end(); ++grid_it){
-        //Print the node id
-        bitConversion( log2(n_size), grid_it->first);
-        cout << ": ";
-        //Print the neighbors
-        for(auto nei_it = grid_it->second.begin(); nei_it != grid_it->second.end(); ++nei_it){
-            bitConversion( log2(n_size), *nei_it);
-            if(nei_it+1 != grid_it->second.end() )
-                cout<<" ";
-        }
-        cout << endl;
-    }
-    return 0;
+    x_ors.clear();
 }
