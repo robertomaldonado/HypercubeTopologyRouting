8ce780e8aa1b76d150a854bead0bb2a52b32a166
Roberto <robertomaldonadog@hotmail.com>
2019-02-03 19:26:48 -0500

    Fixed includes

--- transformations.h
+++ transformations.h
@@ -1,9 +1,8 @@
-#ifndef CHECKER_H_INCLUDED
-#define CHECKER_H_INCLUDED
+#ifndef TRANSFORMATIONS_H_INCLUDED
+#define TRANSFORMATIONS_H_INCLUDED
 #include <iostream>
 #include <map>
 #include <vector>
-
 
 std::string map_to_bin_str(std::map<int, std::vector<int> > &nodes_map , int n_size);
 
@@ -12,6 +11,9 @@ std::string map_to_bin_str(std::map<int, std::vector<int> > &nodes_map , int n_s
 std::string int_to_bin_str(int bit_size, int node);
 
 std::vector<bool> int_to_bin_vector(int bit_size, int node);
+
 std::string int_to_bin_str(int bit_size, int node);
 
-#endif // CHECKER_H_INCLUDED
+int bin_vector_to_int(int bit_size, std::vector<bool >node);
+
+#endif // TRANSFORMATIONS_H_INCLUDED
